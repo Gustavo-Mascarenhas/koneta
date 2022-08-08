cd658096e3c904f29901d79f741fdd3dfda87df3
Giovana Franco Borges <giovana.borges@first-tech.net>
2022-08-08 17:27:49 -0300

    changed date value

--- src/main/java/com/konoPlace/konoplace/models/ReservaModel.java
+++ src/main/java/com/konoPlace/konoplace/models/ReservaModel.java
@@ -27,5 +27,5 @@ public class ReservaModel {
 
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
     @JsonFormat(pattern="dd-MM-yyyy",shape=JsonFormat.Shape.STRING)
-    private Date date;
+    private String date;
 }
