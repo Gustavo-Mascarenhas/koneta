1aa036241496c096bcdb5ae2991c0cb056b33ac0
GiovanaBorges <gigi12345as17@gmail.com>
2022-08-13 19:35:26 -0300

    changed table name for usertable

--- src/main/java/com/konoPlace/konoplace/models/UserModel.java
+++ src/main/java/com/konoPlace/konoplace/models/UserModel.java
@@ -10,7 +10,7 @@ import javax.validation.constraints.NotNull;
 
 import java.util.List;
 
-@Table(name = "userModel")
+@Table(name = "usertable")
 @Entity
 @Data
 public class UserModel {
@@ -23,7 +23,7 @@ public class UserModel {
     private String nome;
 
     @NotNull(message = "O atributo Email é obrigatório")
-    @Column(unique=true)
+    @Column(unique=true, nullable=false)
     private String email;
 
     @NotBlank
