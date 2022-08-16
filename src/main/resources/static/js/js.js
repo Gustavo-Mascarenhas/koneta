3d972e9d8e26bfaab9dd4f12148a9f79d9f0be12
Giovana Franco Borges <giovana.borges@first-tech.net>
2022-08-16 17:27:34 -0300

    upgrade for current version

--- src/main/resources/static/js/js.js
+++ src/main/resources/static/js/js.js
@@ -72,45 +72,6 @@ function date() {
 
 }
 
-function showData(data){
-    console.log(data.length)
-    data.filter(reserva => reserva.length == 0 ? 
-        (
-            document.querySelector(".reserve_space").innerHTML += `
-            <div class="d-flex align-items-center align-content-center mt-5 ">
-              <div class="card m-auto" style="width: 18rem;">
-                <div class="card-body">
-                  <h5 class="card-title" id="reserve_information">Reservas vazias</h5>
-                  <p class="card-text mt-1" id="date_reserve">Você ainda não criou nenhuma reserva</p>
-                  <a href="./home.html" class="btn btn-outline-success" style="width:100%;">Criar reserva</a>
-                </div>
-              </div>
-            </div>
-            `
-        ) :
-    (
-        document.querySelector(".reserve_space").innerHTML += `
-        <div class="d-flex align-items-center align-content-center mt-5 ">
-          <div class="card m-auto" style="width: 18rem;">
-            <div class="card-body">
-              <h5 class="card-title" id="reserve_information">Mesa : ${reserva.mesa.name}</h5>
-              <p class="card-text mt-1" id="date_reserve">Reserva : ${reserva.date}</p>
-              <p class="card-text" id="perifericos">Periféricos: ${reserva.mesa.perifericos}</p>
-              <a href="./home.html" class="btn btn-outline-success" style="width:100%;">Ver informações</a>
-              <div class="d-flex mt-3">
-                <a href="#" data-bs-toggle="modal" data-bs-target="#modalEditReserva" class="btn btn-outline-primary me-1"
-                  style="width:50%;">Editar</a>
-                <a href="#" data-bs-toggle="modal" data-bs-target="#modalDeleteReserva" class="btn btn-outline-danger"
-                  style="width:50%;">Cancelar</a>
-              </div>
-    
-            </div>
-          </div>
-        </div>
-        `
-    )
-    ) 
-}
 
            function checkCookie() {
               console.log(`The cookie is ${Cookies.get('userID')}`)
@@ -126,10 +87,10 @@ function getMesa(places, id) {
 
     function createModal(place) {
       document.querySelector("#place_name").textContent += `Nome da mesa : ${place.name}`
-<!--      document.querySelector("#placeID").placeholder = `${place.id}`-->
+     // document.querySelector("#placeID").placeholder = `${place.id}`
       document.querySelector("#placeID").value = place.id
       document.querySelector("#placeID").textContent += place.id
-<!--      checkCookie()-->
+    //checkCookie()
       document.querySelector("#place_info").textContent += `Periféricos inclusos : ${place.perifericos.map(resp => {
         return ` ${resp} `
           })}`
@@ -143,8 +104,8 @@ function getMesa(places, id) {
 
 
           function cleanModal() {
-          document.querySelector(".place_info").textContent = ""
-          document.querySelector(".place_data").textContent = ""
+           document.querySelector("#place_name").textContent = ""
+           document.querySelector("#place_info").textContent = ""
           }
 
 
@@ -204,8 +165,15 @@ function getMesa(places, id) {
                 id: idUser
               }
             }
-            console.log(`dados : ${JSON.stringify(data)}`)
-            createReserva(data)
+            if(dateInfo == null || dateInfo.length == 0 ){
+                alert("A data não pode ser nula")
+                return
+            }else{
+                 console.log(`dados : ${JSON.stringify(data)}`)
+                        createReserva(data)
+            }
+
+
           }
 
           const createReserva = async (data) => {
@@ -234,6 +202,7 @@ function getMesa(places, id) {
           }
 
           }
+
            function padto2Digits(num) {
           return num.toString().padStart(2, '0');
           }
